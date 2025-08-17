d5e7e215d7679d1ac12d59b70d08eae0af1f5850
Adithya <adithyaaneesh29@gmail>
2025-08-17 21:51:55 +0530

    Add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/home';
+import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import '../styles/Header.css'
 
 const Header = () => {
+
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className='container'>
+            <nav>
+                <div>
+                    <h3>B. Harrison</h3>
+                </div>
+                <div className='menu-item'>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+            
+        </div>
+    </header>
+    
   )
 }
 
